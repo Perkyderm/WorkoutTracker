0a58892894d878dc0fa2c17e348bbc0d8191af67
Perkyderm <ncgoedecke@gmail.com>
2021-03-16 18:50:03 -0600

    update

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,16 +2,15 @@ const app = require("express").Router();
 const path = require("path");
 
 app.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/exercise.html"))
-})
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"))
-})
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 app.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/stats.html"))
-})
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
 
 module.exports = app;
-© 2021 GitHub, Inc.
